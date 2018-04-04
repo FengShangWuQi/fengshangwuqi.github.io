0f35c171c7271b7ef6f69c1a9798195049654873
枫上雾棋 <fengshangwuqi@gmail.com>
2018-04-04 19:44:29 +0800

    fix: 修复 gatsby-plugin-feed 配置 :rose:

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,8 +70,8 @@ module.exports = {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.path,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.path,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ 'content:encoded': edge.node.html }],
                 });
               });
@@ -86,7 +86,7 @@ module.exports = {
                     node {
                       excerpt
                       html
-                      fields { path }
+                      fields { slug }
                       frontmatter {
                         title
 												tag
