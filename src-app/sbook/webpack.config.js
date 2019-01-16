cbae77608748c9a9930106dd740c54ba0582f7c5
fengshangwuqi <fengshangwuqi@gmail.com>
2019-01-16 15:51:36 +0800

    chore: update sb webpack config

--- src-app/sbook/webpack.config.js
+++ src-app/sbook/webpack.config.js
@@ -5,7 +5,10 @@ module.exports = ({ config }) => {
     test: /\.(ts|tsx)$/,
     loader: require.resolve("babel-loader"),
     options: {
-      presets: [["react-app", { flow: false, typescript: true }]],
+      presets: [
+        ["react-app", { flow: false, typescript: true }],
+        "@emotion/babel-preset-css-prop",
+      ],
     },
   });
   config.resolve.extensions.push(".ts", ".tsx");
