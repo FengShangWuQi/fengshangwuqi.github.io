9eb5f95be908ce0fdb18c62e02ef08eb7c482b8e
fengshangwuqi <fengshangwuqi@gmail.com>
2019-01-25 15:32:53 +0800

    style: update DSReset

--- src-core/ds/reset.tsx
+++ src-core/ds/reset.tsx
@@ -2,17 +2,32 @@ import React from "react";
 import { Global } from "@emotion/core";
 import { normalize } from "polished";
 
-export const DSReset = () => (
-  <>
-    <Global styles={{ ...normalize() }} />
+import { useDesignSystem } from "src-core/ds";
 
-    <Global
-      styles={{
-        "ul, li": {
-          margin: 0,
-          padding: 0,
-        },
-      }}
-    />
-  </>
-);
+export const DSReset = () => {
+  const ds = useDesignSystem();
+
+  return (
+    <>
+      <Global styles={{ ...normalize() }} />
+
+      <Global
+        styles={{
+          a: {
+            color: `${ds.color.primary}`,
+            textDecoration: "none",
+          },
+
+          "h1, h2, h3, h4, h5, h6": {
+            margin: 0,
+          },
+
+          "ul, li": {
+            margin: 0,
+            padding: 0,
+          },
+        }}
+      />
+    </>
+  );
+};
