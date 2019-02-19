60da6625bcee0ebfcaeccabc2530b6c4cf13b0ff
fengshangwuqi <fengshangwuqi@gmail.com>
2019-02-19 23:21:15 +0800

    feat: add Header

--- src-components/headers/index.tsx
+++ src-components/headers/index.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import { size } from "src-core/style";
-
+import { useElementRect } from "src-core/react";
 import { useDesignSystem } from "src-core/ds";
+import { size, position } from "src-core/style";
+
+import { Stars } from "src-components/canvas";
 
 export const Header = () => {
   const ds = useDesignSystem();
 
+  const ref = useRef(null);
+  const rect = useElementRect(ref);
+
   return (
     <div
+      ref={ref}
       css={{
-        ...size("100%"),
-        background: "#101012",
+        ...position("relative"),
+        ...size("100%", 400),
+        background: "#171a19",
         color: ds.color.text,
-      }}
-    />
+        cursor: "grab",
+        "&:active": {
+          cursor: "grabbing",
+        },
+      }}>
+      <Stars width={rect.width} height={rect.height} />
+    </div>
   );
 };
