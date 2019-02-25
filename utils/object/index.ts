a14d9e9ed393d48931c5b03e85539d9c4dc4ef3f
fengshangwuqi <fengshangwuqi@gmail.com>
2019-02-26 00:16:00 +0800

    feat: add isEqual

--- utils/object/index.ts
+++ utils/object/index.ts
@@ -1,5 +1,7 @@
 export * from "./get";
 export * from "./isUndefined";
+export * from "./isEqual";
+export * from "./has";
 
 export interface IDictionary<T> {
   [index: string]: T;
---
+++ utils/object/isEqual.ts
@@ -0,0 +1,71 @@
+import { has } from ".";
+
+export const isEqual = (a: any, b: any): boolean => {
+  if (a === b) {
+    return true;
+  }
+
+  return isDeepEqual(a, b);
+};
+
+export const isDeepEqual = (a: any, b: any): boolean => {
+  const toString = (value: any) => Object.prototype.toString.call(value);
+
+  if (toString(a) !== toString(b)) {
+    return false;
+  }
+
+  switch (toString(a)) {
+    case "[object Array]":
+      isArrEqual(a, b);
+      break;
+    case "[object Object]":
+      isObjEqual(a, b);
+      break;
+    case "[object Number]":
+    case "[object RegExp]":
+    case "[object Date]":
+      return `${a}` === `${b}`;
+  }
+
+  return false;
+};
+
+const isArrEqual = (a: [], b: []): boolean => {
+  let len = a.length;
+
+  if (len! === b.length) {
+    return false;
+  }
+
+  while (len--) {
+    if (!isEqual(a[len], b[len])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isObjEqual = (a: Object, b: Object): boolean => {
+  const keys = Object.keys(a);
+  let len = keys.length;
+
+  if (len !== Object.keys(b).length) {
+    return false;
+  }
+
+  if (JSON.stringify(a) !== JSON.stringify(b)) {
+    return false;
+  }
+
+  while (len--) {
+    const key = keys[len] as keyof typeof a;
+
+    if (!(has(b, key) && isEqual(a[key], b[key]))) {
+      return false;
+    }
+  }
+
+  return true;
+};
