43c4a3c5a9bb9389aa04eeb228e1615d0dd409e2
fengshangwuqi <fengshangwuqi@gmail.com>
2019-03-12 13:19:44 +0800

    style: update Title

--- src-app/blog/common/Title.tsx
+++ src-app/blog/common/Title.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { useDesignSystem } from "src-core/ds";
+import { useRect } from "src-core/react";
 import { margin, border } from "src-core/style";
 
 import { rhythm } from "../common/typography";
@@ -8,15 +9,23 @@ import { rhythm } from "../common/typography";
 export const Title = ({ children }: { children: React.ReactNode }) => {
   const ds = useDesignSystem();
 
+  const ref = useRef(null);
+  const rect = useRect(ref);
+
   return (
-    <p
-      css={{
-        ...margin(rhythm(2), 0, rhythm(3 / 4)),
-        ...border("bottom", 3, "solid", ds.color.primary),
-        fontSize: ds.size.l,
-        color: ds.color.text,
-      }}>
+    <h2
+      ref={ref}
+      css={
+        rect.width > ds.grid.m
+          ? {
+              ...margin(rhythm(2), 0, rhythm(3 / 4)),
+              ...border("bottom", 3, "solid", ds.color.primary),
+              fontSize: ds.size.l,
+              color: ds.color.text,
+            }
+          : {}
+      }>
       {children}
-    </p>
+    </h2>
   );
 };
