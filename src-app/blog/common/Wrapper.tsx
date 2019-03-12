8b41d473bc45b2f2ff2cae0ceae37e406d9b7a76
fengshangwuqi <fengshangwuqi@gmail.com>
2019-03-12 15:28:26 +0800

    style: update Wrapper

--- src-app/blog/common/Wrapper.tsx
+++ src-app/blog/common/Wrapper.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 
+import { useDesignSystem } from "src-core/ds";
+import { useRect } from "src-core/react";
 import { padding } from "src-core/style";
 
 import { rhythm } from "./typography";
 
-export const Wrapper = ({ children }: { children: React.ReactNode }) => (
-  <div
-    css={{
-      ...padding(0, 25, rhythm(5 / 2)),
-      boxShadow: "inset 0 0 30px #eee",
-      overflow: "hidden",
-    }}>
-    {children}
-  </div>
-);
+export const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const ds = useDesignSystem();
+
+  const ref = useRef(null);
+  const rect = useRect(ref);
+
+  return (
+    <div
+      ref={ref}
+      css={[
+        {
+          ...padding(0, 25, rhythm(5 / 2)),
+          overflow: "hidden",
+        },
+        rect.width > ds.grid.m ? { boxShadow: "inset 0 0 30px #eee" } : {},
+      ]}>
+      {children}
+    </div>
+  );
+};
