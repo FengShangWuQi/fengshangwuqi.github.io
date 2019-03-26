004e54bb00dfdd482f1ca0be4cc31cd352bf1b50
fengshangwuqi <fengshangwuqi@gmail.com>
2019-03-26 22:56:57 +0800

    refactor: update Route

--- src-core/router/Route.tsx
+++ src-core/router/Route.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 
-export interface IBaseRoute {
-  path?: string;
-  default?: boolean;
-}
-
-export interface IRoute extends IBaseRoute {
-  element: React.ReactElement;
-}
-
-export interface IRouteProps extends IBaseRoute {
-  component: React.ComponentType<any>;
-}
-
-export const Route = ({ component: Component, ...rest }: IRouteProps) => (
-  <Component {...rest} />
-);
+export const Route = ({
+  children,
+}: {
+  path: string;
+  children: React.ReactNode;
+}) => <>{children}</>;
