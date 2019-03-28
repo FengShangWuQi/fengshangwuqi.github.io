abe1bd0f57fd11d212e5e78a89ff2a3ca1a202f0
fengshangwuqi <fengshangwuqi@gmail.com>
2019-03-28 15:13:07 +0800

    feat: add startsWith

--- utils/string/index.ts
+++ utils/string/index.ts
@@ -1,1 +1,3 @@
+export * from "./startsWith";
+
 export * from "./titleCase";
---
+++ utils/string/startsWith.ts
@@ -0,0 +1,2 @@
+export const startsWith = (str: string, str2: string): boolean =>
+  str.substr(0, str2.length) === str2;
