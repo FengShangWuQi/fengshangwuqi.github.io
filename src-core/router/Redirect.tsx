9087f773a73487f0eca3774d53f421a30f4a6cae
fengshangwuqi <fengshangwuqi@gmail.com>
2019-03-28 16:12:23 +0800

    feat: add from in Redirect

--- src-core/router/Redirect.tsx
+++ src-core/router/Redirect.tsx
@@ -6,29 +6,33 @@ import {
   useMatch,
   resolvePath,
   parsePath,
+  ToObj,
 } from "src-core/router";
 
 import { IDictionary } from "utils/object";
 
 export interface IRedirect {
-  to: string;
+  to: string | ToObj;
+  from?: string;
   state?: IDictionary<string>;
 }
 
-export const Redirect = ({ to, state }: IRedirect) => {
-  const { navigateTo = globalHistory.navigateTo } = useLocation();
+export const Redirect = ({ from, to, state }: IRedirect) => {
+  const { location, navigateTo = globalHistory.navigateTo } = useLocation();
   const { uri = "/" } = useMatch();
 
   useEffect(() => {
-    const { pathname, search, hash } = parsePath(to);
+    if (!from || (from && resolvePath(from, uri) === location.pathname)) {
+      const { pathname, search, hash } = parsePath(to);
 
-    const pathTo = resolvePath(pathname as string, uri);
-    const redirectTo = `${pathTo}${search}${hash}`;
+      const pathTo = resolvePath(pathname as string, uri);
+      const redirectTo = `${pathTo}${search}${hash}`;
 
-    navigateTo(redirectTo, {
-      state: state || { key: `${Date.now()}-redirect` },
-      replace: true,
-    });
+      navigateTo(redirectTo, {
+        state: state || { key: `${Date.now()}-redirect` },
+        replace: true,
+      });
+    }
   }, []);
 
   return null;
