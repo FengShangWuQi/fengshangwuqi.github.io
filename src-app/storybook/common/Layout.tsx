99a81009f43edb26130447f60e806e19781b3200
fengshangwuqi <fengshangwuqi@gmail.com>
2019-04-10 23:48:37 +0800

    style(sb): update Layout

--- src-app/storybook/common/Layout.tsx
+++ src-app/storybook/common/Layout.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Global } from "@emotion/core";
 
-import { defaultTheme } from "src-core/ds";
+import { defaultTheme, useDesignSystem } from "src-core/ds";
+
+import { rhythm, margin, border } from "src-core/style";
 
 export const storybookTheme = {
   ...defaultTheme,
   color: {
     ...defaultTheme.color,
     primary: "#c2185b",
+    text: "#1B1D1D",
   },
 };
 
 export const StorybookGlobal = () => {
+  const ds = useDesignSystem();
+
   return (
     <Global
       styles={{
@@ -19,6 +24,16 @@ export const StorybookGlobal = () => {
           width: "100%",
           height: "100%",
           background: "#fafafa",
+        },
+
+        h1: {
+          marginBottom: rhythm(1),
+        },
+
+        h3: {
+          ...margin(rhythm(3 / 2), 0, rhythm(1)),
+          ...border("bottom", 1, "solid", ds.color.primary),
+          paddingBottom: rhythm(1 / 2),
         },
       }}
     />
