9ed0766736d4d0081b7d7f2e9b33e67f42cbfa1c
fengshangwuqi <fengshangwuqi@gmail.com>
2019-04-10 23:46:01 +0800

    feat(sb): add tag

--- src-components/menus/__storybook__/BaseMenu.tsx
+++ src-components/menus/__storybook__/BaseMenu.tsx
@@ -1,9 +1,50 @@
 import React from "react";
 
-import { BaseMenu } from "..";
+import { BaseMenu, BaseMenuItem, MenuMode } from "..";
 
 export default () => (
   <div>
-    <BaseMenu>this is BaseMenu.</BaseMenu>
+    <h3>HORIZONTAL</h3>
+
+    <BaseMenu
+      css={{
+        color: "#ffffff",
+        background: "#009688",
+        height: 60,
+        lineHeight: "60px",
+      }}>
+      {["docs", "blog", "github"].map(item => (
+        <BaseMenuItem>
+          <a
+            href="#"
+            css={{
+              color: "#ffffff",
+            }}>
+            {item.toUpperCase()}
+          </a>
+        </BaseMenuItem>
+      ))}
+    </BaseMenu>
+
+    <h3>VERTICAL</h3>
+
+    <BaseMenu
+      css={{
+        color: "#ffffff",
+        background: "#009688",
+      }}
+      mode={MenuMode.VERTICAL}>
+      {["docs", "blog", "github"].map(item => (
+        <BaseMenuItem>
+          <a
+            href="#"
+            css={{
+              color: "#ffffff",
+            }}>
+            {item.toUpperCase()}
+          </a>
+        </BaseMenuItem>
+      ))}
+    </BaseMenu>
   </div>
 );
--- src-components/tags/__storybook__/Tag.tsx
+++ src-components/tags/__storybook__/Tag.tsx
@@ -4,8 +4,22 @@ import { Tag } from "../";
 
 export default () => (
   <div>
-    <Tag color="#ffffff" bg="#004f86">
-      123
+    <Tag
+      color="#61DAFB"
+      bg="#222222"
+      css={{
+        marginRight: 8,
+        fontWeight: "bold",
+      }}>
+      React
+    </Tag>
+    <Tag
+      color="#ffffff"
+      bg="#004f86"
+      css={{
+        fontWeight: "bold",
+      }}>
+      CSS
     </Tag>
   </div>
 );
