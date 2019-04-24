26a6402151c21f74951c3bbd3bf7102a82c46e76
fengshangwuqi <fengshangwuqi@gmail.com>
2019-04-24 16:05:16 +0800

    perf: update isObj

--- utils/object/isObject.ts
+++ utils/object/isObject.ts
@@ -3,5 +3,5 @@ import { isNull } from "utils/object";
 export const isObject = (value: any) => {
   const type = typeof value;
 
-  return (type == "object" || type == "function") && !isNull(value);
+  return (type === "object" || type === "function") && !isNull(value);
 };
