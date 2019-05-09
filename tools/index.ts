ad9a9ade91c8376b8d65d2f5714761bf4105beab
fengshangwuqi <fengshangwuqi@gmail.com>
2019-05-09 14:18:14 +0800

    perf: update tool start

--- tools/index.ts
+++ tools/index.ts
@@ -1,4 +1,4 @@
-import { run } from "./spawn";
+import { run } from "./run";
 
 enum Apps {
   bg = "blog",
@@ -12,14 +12,32 @@ enum Actions {
 }
 
 enum Branchs {
+  DEV = "dev",
   MASTER = "master",
   GHPAGES = "gh-pages",
 }
 
-const APP_ENVS = (app: string) => ({
-  APP: app,
-  APP_BRANCH: app === Apps.bg ? Branchs.MASTER : Branchs.GHPAGES,
-});
+const APP_ENVS = (app: string, action: string) => {
+  const env = { APP: app };
+
+  switch (action) {
+    case Actions.dev: {
+      return {
+        ...env,
+        BRANCH: Branchs.DEV,
+      };
+    }
+    case Actions.r: {
+      return {
+        ...env,
+        BRANCH: app === Apps.bg ? Branchs.MASTER : Branchs.GHPAGES,
+      };
+    }
+    default: {
+      return env;
+    }
+  }
+};
 
 (() => {
   const apps: string[] = [Apps.bg, Apps.sb];
@@ -28,7 +46,7 @@ const APP_ENVS = (app: string) => ({
   const app = Apps[process.argv[2] as keyof typeof Apps] || apps[0];
   const action = Actions[process.argv[3] as keyof typeof Actions] || actions[0];
 
-  const envs = APP_ENVS(app);
+  const envs = APP_ENVS(app, action);
   console.log({ ...envs, ACTION: action });
 
   run(action, { ...envs });
