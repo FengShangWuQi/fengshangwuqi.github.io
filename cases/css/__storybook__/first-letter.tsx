df63f97c74955ce92c65493f840db3b479986a4b
fengshangwuqi <fengshangwuqi@gmail.com>
2019-05-28 11:21:44 +0800

    feat(sb): add first-letter

---
+++ cases/css/__storybook__/first-letter.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { EditLink } from "src-app/storybook/common/Storybook";
+
+export default () => {
+  return (
+    <div
+      css={{
+        "& p": {
+          marginTop: 24,
+          fontSize: 16,
+          lineHeight: 1.625,
+
+          "&:first-of-type::first-letter": {
+            float: "left",
+            paddingRight: 10,
+            lineHeight: 1.1,
+            fontSize: 48,
+            color: "#cd5c5c",
+          },
+        },
+      }}>
+      <p>
+        The ::first-letter CSS pseudo-element applies styles to the first letter
+        of the first line of a block-level element, but only when not preceded
+        by other content (such as images or inline tables). The ::first-letter
+        CSS pseudo-element applies styles to the first letter of the first line
+        of a block-level element, but only when not preceded by other content
+        (such as images or inline tables).
+      </p>
+
+      <p>
+        The ::first-letter CSS pseudo-element applies styles to the first letter
+        of the first line of a block-level element, but only when not preceded
+        by other content (such as images or inline tables).
+      </p>
+
+      <EditLink path="cases/css/__storybook__/first-letter.tsx" />
+    </div>
+  );
+};
