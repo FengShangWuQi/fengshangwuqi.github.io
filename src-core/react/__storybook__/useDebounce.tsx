a8de2dcd87df206c223695994343211f7381bc3b
fengshangwuqi <fengshangwuqi@gmail.com>
2019-06-13 16:49:17 +0800

    feat(sb): add useDebounce

---
+++ src-core/react/__storybook__/useDebounce.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import { EditLink } from "src-app/storybook/common/Storybook";
+
+import { useDebounce } from "../";
+
+export default () => {
+  const [count, setCount] = useState(0);
+  const debounceCount = useDebounce(count, 200);
+
+  return (
+    <div>
+      <div>
+        <div>count: {count}</div>
+        <div>debounceCount: {debounceCount}</div>
+      </div>
+
+      <button
+        css={{
+          marginTop: 24,
+        }}
+        onClick={() => {
+          setCount(count + 1);
+        }}>
+        click
+      </button>
+
+      <EditLink path="src-core/react/useDebounce.ts" />
+    </div>
+  );
+};
--- src-core/rxjs/index.ts
+++ src-core/rxjs/index.ts
@@ -2,7 +2,10 @@ export { Observable as rxObservable } from "rxjs";
 export { BehaviorSubject as rxBehaviorSubject } from "rxjs";
 export { fromEvent as rxFromEvent } from "rxjs";
 export { merge as rxMerge } from "rxjs";
+export { interval as rxInterval } from "rxjs";
+export { of as rxOf } from "rxjs";
 
 export { debounceTime as rxDebounceTime } from "rxjs/operators";
 export { filter as rxFilter } from "rxjs/operators";
 export { delay as rxDelay } from "rxjs/operators";
+export { take as rxTake } from "rxjs/operators";
