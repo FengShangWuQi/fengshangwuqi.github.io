9affc5b24e9d63afa80b72e774dfa477154823e2
fengshangwuqi <fengshangwuqi@gmail.com>
2019-09-21 10:53:05 +0800

    perf(sb): update border

--- src-core/style/__storybook__/border.tsx
+++ src-core/style/__storybook__/border.tsx
@@ -2,21 +2,12 @@ import React from "react";
 
 import { EditLink } from "src-app/storybook/common/Storybook";
 
-import { border } from "..";
-
 export default () => {
   return (
     <div>
       <pre>{`{
   ...border("bottom", 1, "solid", "#eee")
 }`}</pre>
-
-      <div
-        css={{
-          ...border("bottom", 1, "solid", "#eee"),
-          marginTop: 24,
-        }}
-      />
 
       <EditLink path="src-core/style/border.ts" />
     </div>
