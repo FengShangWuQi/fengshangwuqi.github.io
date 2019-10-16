6752d5d33b3f18f1a8c7745cb155ecaf2eb64113
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-16 17:09:02 +0800

    feat(utils): randomNum

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,6 @@ export const sleep = (interval: number) => {
     setTimeout(resolve, interval);
   });
 };
+
+export const randomNum = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
