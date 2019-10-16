cf732fdbe96747d2660e0c9af461aa4ab83570f0
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-16 17:15:00 +0800

    feat(core): add colorPalette

---
+++ src-core/ds/__storybook__/colorPalette.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { lighten, darken, rgba } from "polished";
+
+import { pickElmAttrs } from "src-core/react";
+import { useDesignSystem } from "src-core/ds";
+import { flex, border } from "src-core/style";
+
+import { EditLink } from "src-app/storybook/common/Storybook";
+
+import { randomNum } from "utils";
+
+import { colorPalette } from "../colorPalette";
+
+export default () => {
+  const ds = useDesignSystem();
+
+  return (
+    <div>
+      {Object.keys(colorPalette).map(color => {
+        const value = colorPalette[color as keyof typeof colorPalette];
+
+        return (
+          <div>
+            <div
+              css={{
+                marginTop: ds.padding.l,
+                marginBottom: ds.padding.s,
+                fontSize: ds.size.s,
+              }}>
+              {color.toUpperCase()}
+            </div>
+            {color === "black" || color === "white" ? (
+              <ColorBlock
+                css={{
+                  ...border(1, "solid", rgba(colorPalette.black, 0.12)),
+                  background: value,
+                }}
+              />
+            ) : (
+              <ColorGroup color={value} />
+            )}
+          </div>
+        );
+      })}
+      <EditLink path="src-core/ds/colorPalette.ts" />
+    </div>
+  );
+};
+
+const ColorGroup = ({ color }: { color: string }) => {
+  const colorArr = [
+    darken(0.2, color),
+    darken(0.15, color),
+    darken(0.1, color),
+    darken(0.05, color),
+    color,
+    lighten(0.05, color),
+    lighten(0.1, color),
+    lighten(0.15, color),
+    lighten(0.2, color),
+    lighten(0.25, color),
+    lighten(0.3, color),
+  ];
+
+  const randomIndex = randomNum(0, 10);
+
+  return (
+    <div
+      css={{
+        ...flex({}),
+      }}>
+      {colorArr.map((background, index) => (
+        <ColorBlock
+          css={{
+            ...(index === randomIndex && {
+              borderRadius: "50%",
+            }),
+            background,
+          }}></ColorBlock>
+      ))}
+    </div>
+  );
+};
+
+const ColorBlock = ({ ...otherProps }) => (
+  <div
+    {...pickElmAttrs(otherProps)}
+    css={{
+      width: 64,
+      height: 64,
+    }}></div>
+);
--- src-core/ds/index.tsx
+++ src-core/ds/index.tsx
@@ -1,5 +1,5 @@
 export { ThemeProvider } from "emotion-theming";
+export * from "./colorPalette";
 export * from "./defaultTheme";
 export * from "./reset";
 export * from "./useDesignSystem";
-export * from "./toggleTheme";
