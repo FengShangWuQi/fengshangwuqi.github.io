d7fdb7a1a2c657a885e25ab6c72cb813ad2a8332
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-16 17:16:18 +0800

    fix: rm toggle theme

--- src-components/toggles/__storybook__/Toggle.tsx
+++ src-components/toggles/__storybook__/Toggle.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 
-import { useToglleTheme } from "src-core/ds";
-
 import { EditLink } from "src-app/storybook/common/Storybook";
 
 import { Toggle } from "..";
 
 export default () => {
-  const { theme, toggleTheme } = useToglleTheme();
-
   return (
     <div>
       <Toggle
         css={{
           marginLeft: 3,
         }}
-        defaultChecked={theme === "dark"}
         icons={{
           checked: (
             <img src={require("../images/moon.png")} width="16" height="16" />
@@ -24,7 +19,6 @@ export default () => {
             <img src={require("../images/sun.png")} width="16" height="16" />
           ),
         }}
-        onChange={toggleTheme}
       />
 
       <EditLink path="src-components/toggles/Toggle.tsx" />
--- src-core/react/Bootstrap.tsx
+++ src-core/react/Bootstrap.tsx
@@ -1,14 +1,7 @@
 import React, { StrictMode } from "react";
 
-import {
-  ThemeProvider,
-  ToggleThemeProvider,
-  DSReset,
-  ITheme,
-} from "src-core/ds";
+import { ThemeProvider, DSReset, ITheme } from "src-core/ds";
 import { Location } from "src-core/router";
-
-import { useLocalStorage } from ".";
 
 export const Bootstrap = ({
   ds,
@@ -17,24 +10,13 @@ export const Bootstrap = ({
   ds: ITheme;
   children: React.ReactNode;
 }) => {
-  const [theme, setTheme] = useLocalStorage("theme", "light");
-
-  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
-  const dsTheme = theme === "light" ? ds : ds.reverse();
-
   return (
     <StrictMode>
       <Location>
-        <ToggleThemeProvider
-          value={{
-            theme,
-            toggleTheme,
-          }}>
-          <ThemeProvider theme={dsTheme}>
-            <DSReset />
-            {children}
-          </ThemeProvider>
-        </ToggleThemeProvider>
+        <ThemeProvider theme={ds}>
+          <DSReset />
+          {children}
+        </ThemeProvider>
       </Location>
     </StrictMode>
   );
