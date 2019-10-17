b6d4b39e83a531133e22305872624306da5d99fa
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-17 20:53:55 +0800

    feat(sb): add merge

---
+++ utils/object/__storybook__/merge.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { EditLink } from "src-app/storybook/common/Storybook";
+
+export default () => {
+  return (
+    <div>
+      <pre>
+        {`const object = {
+    a: [{ b: 2 }, { d: 4 }],
+};
+
+const other = {
+    a: [{ c: 3 }, { e: 5 }],
+};
+
+merge(object, other);
+// => { 'a': [{ 'b': 2, 'c': 3 }, { 'd': 4, 'e': 5 }] }
+`}
+      </pre>
+
+      <EditLink path="utils/object/merge.ts" />
+    </div>
+  );
+};
