523469b3fb7e5425cc8847033945f9b78a32fde2
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-20 23:47:24 +0800

    feat(sb): add withoutBubble

---
+++ src-core/react/__storybook__/withoutBubble.md
@@ -0,0 +1,19 @@
+---
+group: core
+module: react
+title: withoutBubble
+---
+
+import { Source } from "src-app/storybook/common/Source";
+
+import { WithoutBubbleDemo } from "./withoutBubble";
+
+<WithoutBubbleDemo />
+
+```jsx
+<a href="#!" onClick={withoutBubble(() => {})}>
+  Link
+</a>
+```
+
+<Source path="src-core/react/withoutBubble.ts" />
--- src-core/react/__storybook__/withoutBubble.tsx
+++ src-core/react/__storybook__/withoutBubble.tsx
@@ -2,21 +2,14 @@ import React from "react";
 
 import { withoutBubble } from "..";
 
-export default () => {
+export const WithoutBubbleDemo = () => {
   return (
-    <div
-      onClick={() => {
-        console.log("Bubble");
-      }}>
-      <a
-        href="#!"
-        onClick={withoutBubble(() => {
-          console.log("withoutBubble");
-        })}>
-        Link
-      </a>
-    </div>
-
-    // <EditLink path="src-core/react/withoutBubble.ts" />
+    <a
+      href="#!"
+      onClick={withoutBubble(() => {
+        console.log("withoutBubble");
+      })}>
+      Link
+    </a>
   );
 };
