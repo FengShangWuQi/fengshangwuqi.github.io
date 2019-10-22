1cfecac53291653e050a27f52505a0b90dc40272
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-22 23:20:52 +0800

    feat(style): add inlineFlex

--- src-core/style/flex.ts
+++ src-core/style/flex.ts
@@ -4,3 +4,8 @@ export const flex = (flexOpts: CSSObject): CSSObject => ({
   display: "flex",
   ...flexOpts,
 });
+
+export const inlineFlex = (flexOpts: CSSObject): CSSObject => ({
+  display: "inline-flex",
+  ...flexOpts,
+});
