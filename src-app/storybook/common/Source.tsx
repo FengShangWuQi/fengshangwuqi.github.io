3fd4f696d5d4953e3bd3a9054a6106b7ac4f08be
fengshangwuqi <fengshangwuqi@gmail.com>
2019-10-22 23:23:00 +0800

    perf(storybook): update Source

--- src-app/storybook/common/Source.tsx
+++ src-app/storybook/common/Source.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 
 import { useDesignSystem } from "src-core/ds";
-import { rhythm } from "src-core/style";
+import { inlineFlex, rhythm } from "src-core/style";
+
+import { IconGithubFill } from "src-components/basic/Icon";
 
 export const Source = ({ path }: { path: string }) => {
   const ds = useDesignSystem();
 
   return (
-    <div
+    <a
       css={{
+        ...inlineFlex({
+          alignItems: "center",
+        }),
         marginTop: rhythm(2),
         fontSize: ds.size.xs,
-      }}>
-      <a
-        href={`https://github.com/FengShangWuQi/fengshangwuqi.github.io/blob/dev/${path}`}
-        target="_blank"
-        rel="noopener noreferrer">
-        Source
-      </a>
-    </div>
+      }}
+      href={`https://github.com/FengShangWuQi/fengshangwuqi.github.io/blob/dev/${path}`}
+      target="_blank"
+      rel="noopener noreferrer">
+      <IconGithubFill
+        css={{
+          marginRight: ds.padding.s,
+          fill: ds.color.primary,
+        }}
+      />
+      <span>Source</span>
+    </a>
   );
 };
