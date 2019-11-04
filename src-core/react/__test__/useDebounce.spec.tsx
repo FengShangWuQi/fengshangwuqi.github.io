84ed49ae0e86787a805e72855b59d741d6a33296
fengshangwuqi <fengshangwuqi@gmail.com>
2019-11-04 17:19:58 +0800

    chore: add react hooks test

---
+++ src-core/react/__test__/useDebounce.spec.tsx
@@ -0,0 +1,21 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import { useDebounce } from "../useDebounce";
+
+describe("useDebounce #core #react", () => {
+  it("useDebounce", async () => {
+    let count = 0;
+    const { result, rerender, waitForNextUpdate } = renderHook(() =>
+      useDebounce(count, 200),
+    );
+
+    count = 20;
+    rerender();
+
+    expect(result.current).toBe(0);
+
+    await waitForNextUpdate();
+
+    expect(result.current).toBe(20);
+  });
+});
