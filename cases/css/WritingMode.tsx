c3d3d4621f8127bbf48ac80f788926585fefc6ce
fengshangwuqi <fengshangwuqi@gmail.com>
2019-11-05 21:40:54 +0800

    feat(storybook): add WritingMode

---
+++ cases/css/WritingMode.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { clearFix } from "polished";
+
+import { pickElmAttrs } from "src-core/react";
+import { useDesignSystem } from "src-core/ds";
+import { flex } from "src-core/style";
+
+export const HorizontalTb = () => (
+  <div
+    css={{
+      ...flex({ justifyContent: "center" }),
+    }}>
+    <Poetry
+      css={{
+        textAlign: "center",
+        writingMode: "horizontal-tb",
+      }}
+    />
+  </div>
+);
+
+export const VerticalLr = () => <Poetry css={{ writingMode: "vertical-lr" }} />;
+
+export const VerticalRl = () => (
+  <div
+    css={{
+      ...clearFix(),
+    }}>
+    <Poetry css={{ float: "right", writingMode: "vertical-rl" }} />
+  </div>
+);
+
+const Poetry = ({ ...otherProps }) => {
+  const ds = useDesignSystem();
+
+  return (
+    <div
+      {...pickElmAttrs(otherProps)}
+      css={{
+        "& p": {
+          margin: ds.padding.l,
+        },
+      }}>
+      <p>《水调歌头·明月几时有》</p>
+      <p>明月几时有</p>
+      <p>把酒问青天</p>
+      <p>不知天上宫阙</p>
+      <p>今夕是何年</p>
+      <p>我欲乘风归去</p>
+      <p>又恐琼楼玉宇</p>
+      <p>高处不胜寒</p>
+      <p>起舞弄清影</p>
+      <p>何似在人间</p>
+      <p>转朱阁</p>
+      <p>低绮户</p>
+      <p>照无眠</p>
+      <p>不应有恨</p>
+      <p>何事长向别时圆</p>
+      <p>人有悲欢离合</p>
+      <p>月有阴晴圆缺</p>
+      <p>此事古难全</p>
+      <p>但愿人长久</p>
+      <p>千里共婵娟</p>
+      <p>-- 苏轼</p>
+    </div>
+  );
+};
---
+++ cases/css/__storybook__/WritingMode.md
@@ -0,0 +1,19 @@
+---
+group: cases
+module: css
+title: WritingMode
+---
+
+import { Source } from "src-app/storybook/common/Source";
+
+import { VerticalRl } from "../WritingMode"
+
+<VerticalRl />
+
+```jsx {2}
+const WritingMode = styled.div({
+  writingMode: "vertical-rl",
+});
+```
+
+<Source path="cases/css/WritingMode.tsx" />
