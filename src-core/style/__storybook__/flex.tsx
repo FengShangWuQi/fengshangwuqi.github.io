ac4557e89ea77ece264d053637bbfe311b749838
fengshangwuqi <fengshangwuqi@gmail.com>
2019-11-06 17:24:30 +0800

    feat(sb): add flex

--- src-core/style/__storybook__/flex.tsx
+++ src-core/style/__storybook__/flex.tsx
@@ -1,36 +1,23 @@
 import React from "react";
 
-import { flex } from "..";
+import { flex } from "../flex";
 
-export const FlexDemo = () => {
-  return (
-    <div>
-      <pre>{`{
-  ...flex({
-    justifyContent: "center",
-    alignItems: "center",
-  })
-}`}</pre>
-
-      <div
-        css={{
-          ...flex({
-            justifyContent: "center",
-            alignItems: "center",
-          }),
-          marginTop: 24,
-          width: 620,
-          height: 320,
-          fontSize: 24,
-          color: "white",
-          background: "#ff8c00",
-          borderRadius: 8,
-          border: "1px solid #eee",
-        }}>
-        CSS FlexBox
-      </div>
-
-      {/* <EditLink path="src-core/style/flex.ts" /> */}
-    </div>
-  );
-};
+export const FlexDemo = () => (
+  <div
+    css={{
+      ...flex({
+        justifyContent: "center",
+        alignItems: "center",
+      }),
+      marginTop: 24,
+      width: 620,
+      height: 320,
+      fontSize: 24,
+      color: "white",
+      background: "#ff8c00",
+      borderRadius: 8,
+      border: "1px solid #eee",
+    }}>
+    CSS FlexBox
+  </div>
+);
