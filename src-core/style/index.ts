e71a02e6461c39bf0e648ff98989f0528c7c6796
fengshangwuqi <fengshangwuqi@gmail.com>
2019-11-06 16:45:36 +0800

    perf: rm directionalProperty

--- src-core/style/index.ts
+++ src-core/style/index.ts
@@ -1,34 +1,3 @@
-import { CSSObject } from "@emotion/core";
-
-import { titleCase } from "utils/string";
-
-export const sides = ["top", "right", "bottom", "left"];
-
-export const directionalProperty = (
-  property: string,
-  ...values: Array<string | number>
-): CSSObject => {
-  if (values.length === 0) return {};
-
-  const [
-    firstValue,
-    secondValue = firstValue,
-    thirdValue = firstValue,
-    fourthValue = secondValue,
-  ] = values;
-  const defaultValue = [firstValue, secondValue, thirdValue, fourthValue];
-  const directionalStyle: CSSObject = {};
-
-  defaultValue.forEach((value, index) => {
-    const key = property
-      ? `${property}${titleCase(sides[index])}`
-      : sides[index];
-    directionalStyle[key] = value;
-  });
-
-  return directionalStyle;
-};
-
 export * from "./flex";
 export * from "./grid";
 export * from "./typography";
