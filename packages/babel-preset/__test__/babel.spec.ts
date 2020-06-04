8200591a41765fb0ee2f3d5e67f9a35487308c15
fengshangwuqi <fengshangwuqi@gmail.com>
2020-06-04 12:52:13 +0800

    perf: update babel test

--- packages/babel-preset/__test__/babel.spec.ts
+++ packages/babel-preset/__test__/babel.spec.ts
@@ -12,15 +12,28 @@ describe("transform #babel", () => {
     const code = `const a: number = 1;`;
     const result = transform(code, babelOpts);
 
-    expect(result?.code).toBe(`"use strict";\n\nconst a = 1;`);
+    expect(result?.code).toMatch(`const a = 1`);
   });
 
   it("transform class", () => {
-    const code = `class Book {}`;
+    const code = `class Book {};`;
     const result = transform(code, babelOpts);
 
-    console.log(result);
+    expect(result?.code).toMatch(`class Book {}`);
+  });
 
-    expect(result?.code).toBe(`"use strict";\n\nclass Book {};`);
+  it("transform optional-chaining", () => {
+    const code = `foo?.bar;`;
+    const result = transform(code, babelOpts);
+
+    expect(result?.code).toMatch(`=== void 0 ? void 0 :`);
+  });
+
+  it("transform nullish-coalescing-operator", () => {
+    const code = `object.foo ?? "default";`;
+    const result = transform(code, babelOpts);
+
+    expect(result?.code).toMatch(`!== null &&`);
+    expect(result?.code).toMatch(`!== void 0 ?`);
   });
 });
