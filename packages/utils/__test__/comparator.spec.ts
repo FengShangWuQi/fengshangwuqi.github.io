1f29cba591e43ff50f8ffc25c8cbb374b24dd4d7
fengshangwuqi <fengshangwuqi@gmail.com>
2020-06-21 13:07:27 +0800

    fix: comparator test

--- packages/utils/__test__/comparator.spec.ts
+++ packages/utils/__test__/comparator.spec.ts
@@ -1,22 +1,22 @@
-import * as Comparator from "../src/comparator";
+import { comparator } from "../src/comparator";
 
-describe("Comparator #utils", () => {
+describe("comparator #utils", () => {
   it("should compare with comparator function", () => {
-    expect(Comparator.equal(0, 0)).toBe(true);
-    expect(Comparator.equal(0, 1)).toBe(false);
-    expect(Comparator.equal("a", "a")).toBe(true);
-    expect(Comparator.lessThan(1, 2)).toBe(true);
-    expect(Comparator.lessThan(-1, 2)).toBe(true);
-    expect(Comparator.lessThan("a", "b")).toBe(true);
-    expect(Comparator.lessThan("a", "ab")).toBe(true);
-    expect(Comparator.lessThan(10, 2)).toBe(false);
-    expect(Comparator.lessThanOrEqual(10, 2)).toBe(false);
-    expect(Comparator.lessThanOrEqual(1, 1)).toBe(true);
-    expect(Comparator.lessThanOrEqual(0, 0)).toBe(true);
-    expect(Comparator.greaterThan(0, 0)).toBe(false);
-    expect(Comparator.greaterThan(10, 0)).toBe(true);
-    expect(Comparator.greaterThanOrEqual(10, 0)).toBe(true);
-    expect(Comparator.greaterThanOrEqual(10, 10)).toBe(true);
-    expect(Comparator.greaterThanOrEqual(0, 10)).toBe(false);
+    expect(comparator.equal(0, 0)).toBe(true);
+    expect(comparator.equal(0, 1)).toBe(false);
+    expect(comparator.equal("a", "a")).toBe(true);
+    expect(comparator.lessThan(1, 2)).toBe(true);
+    expect(comparator.lessThan(-1, 2)).toBe(true);
+    expect(comparator.lessThan("a", "b")).toBe(true);
+    expect(comparator.lessThan("a", "ab")).toBe(true);
+    expect(comparator.lessThan(10, 2)).toBe(false);
+    expect(comparator.lessThanOrEqual(10, 2)).toBe(false);
+    expect(comparator.lessThanOrEqual(1, 1)).toBe(true);
+    expect(comparator.lessThanOrEqual(0, 0)).toBe(true);
+    expect(comparator.greaterThan(0, 0)).toBe(false);
+    expect(comparator.greaterThan(10, 0)).toBe(true);
+    expect(comparator.greaterThanOrEqual(10, 0)).toBe(true);
+    expect(comparator.greaterThanOrEqual(10, 10)).toBe(true);
+    expect(comparator.greaterThanOrEqual(0, 10)).toBe(false);
   });
 });
