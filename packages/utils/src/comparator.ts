76a48d34fceceda70229cdd23e14b8dec2ae5894
fengshangwuqi <fengshangwuqi@gmail.com>
2020-06-21 12:55:22 +0800

    perf: update comparator

--- packages/utils/src/comparator.ts
+++ packages/utils/src/comparator.ts
@@ -1,26 +1,34 @@
-const comparator = <T>(a: T, b: T): number => {
+const comparatorFun = <T>(a: T, b: T): number => {
   if (a === b) {
     return 0;
   }
   return a > b ? 1 : -1;
 };
 
-export const equal = <T>(a: T, b: T): boolean => {
-  return comparator(a, b) === 0;
+const equal = <T>(a: T, b: T): boolean => {
+  return comparatorFun(a, b) === 0;
 };
 
-export const lessThan = <T>(a: T, b: T): boolean => {
-  return comparator(a, b) < 0;
+const lessThan = <T>(a: T, b: T): boolean => {
+  return comparatorFun(a, b) < 0;
 };
 
-export const greaterThan = <T>(a: T, b: T): boolean => {
-  return comparator(a, b) > 0;
+const greaterThan = <T>(a: T, b: T): boolean => {
+  return comparatorFun(a, b) > 0;
 };
 
-export const lessThanOrEqual = <T>(a: T, b: T): boolean => {
+const lessThanOrEqual = <T>(a: T, b: T): boolean => {
   return lessThan(a, b) || equal(a, b);
 };
 
-export const greaterThanOrEqual = <T>(a: T, b: T): boolean => {
+const greaterThanOrEqual = <T>(a: T, b: T): boolean => {
   return greaterThan(a, b) || equal(a, b);
+};
+
+export const comparator = {
+  equal,
+  lessThan,
+  greaterThan,
+  lessThanOrEqual,
+  greaterThanOrEqual,
 };
