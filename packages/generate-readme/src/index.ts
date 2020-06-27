72100315d75097d2845288e173f3aac6a76f3235
fengshangwuqi <fengshangwuqi@gmail.com>
2020-06-27 22:20:30 +0800

    perf: update generate readme

--- packages/generate-readme/src/index.ts
+++ packages/generate-readme/src/index.ts
@@ -1,23 +1,2 @@
-import { fromFrontMatter } from "./fromFrontMatter";
-import { fromMindnode } from "./fromMindnode";
-import chalk from "chalk";
-
-export enum README_TYPE {
-  FRONT_MATTER = "FRONT_MATTER",
-  MINDNODE = "MINDNODE",
-}
-
-export const generateReadme = (from: keyof typeof README_TYPE) => {
-  switch (from) {
-    case README_TYPE.FRONT_MATTER: {
-      return fromFrontMatter();
-    }
-    case README_TYPE.MINDNODE: {
-      return fromMindnode();
-    }
-    default:
-      return "";
-  }
-
-  console.log(chalk.green("success".toUpperCase()), "generate readme");
-};
+export * from "./fromFrontMatter";
+export * from "./fromMindnode";
