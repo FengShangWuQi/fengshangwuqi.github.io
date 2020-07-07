d746c00ab4f8df94cd0839e506550fc061321b95
fengshangwuqi <fengshangwuqi@gmail.com>
2020-07-07 16:03:36 +0800

    perf(pkg): update eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -28,8 +28,10 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-unsafe-return": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/no-this-alias": 0,
     "@typescript-eslint/no-unsafe-assignment": 1,
     "@typescript-eslint/no-var-requires": 1,
+    "@typescript-eslint/ban-ts-comment": 1,
     "@typescript-eslint/no-floating-promises": [2, { ignoreIIFE: true }],
   },
   settings: {
