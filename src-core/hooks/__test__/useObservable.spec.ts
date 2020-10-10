e2f58d30b3cd1020d84cfa1d7facb136a2a0106c
fengshangwuqi <fengshangwuqi@gmail.com>
2020-10-10 18:36:48 +0800

    fix: ts err

--- src-core/hooks/__test__/useObservable.spec.ts
+++ src-core/hooks/__test__/useObservable.spec.ts
@@ -7,15 +7,15 @@ describe("useObservable #core #hooks", () => {
   it("when add default value 1 - with default value 1", () => {
     const number$ = new Subject<number>();
 
-    const { result } = renderHook(() => useObservable<number>(number$, 1));
+    const { result } = renderHook(() => useObservable(number$, 1));
 
     expect(result.current).toBe(1);
   });
 
   it("when add 1 - with value 1", () => {
-    const number$ = new BehaviorSubject<number>(1);
+    const number$ = new BehaviorSubject(1);
 
-    const { result } = renderHook(() => useObservable<number>(number$));
+    const { result } = renderHook(() => useObservable(number$, number$.value));
 
     expect(result.current).toBe(1);
   });
