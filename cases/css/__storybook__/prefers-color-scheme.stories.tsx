e2d8a0a1cdf8fa6987b994bf60f4adb5ece96121
fengshangwuqi <fengshangwuqi@gmail.com>
2020-10-12 12:31:21 +0800

    docs(sb): prefers-color-scheme

--- cases/css/__storybook__/prefers-color-scheme.stories.tsx
+++ cases/css/__storybook__/prefers-color-scheme.stories.tsx
@@ -14,11 +14,11 @@ export const PrefersColorSchemeDemo = () => {
         fontSize: ds.size["2xl"],
         "@media (prefers-color-scheme: dark)": {
           color: ds.colorPalette.white,
-          background: "#232323",
+          background: ds.colorPalette.teal[800],
         },
         "@media (prefers-color-scheme: light)": {
-          ...border(1, "solid", "#232323"),
-          color: ds.color.textLight,
+          ...border(1, "solid", ds.colorPalette.black),
+          color: ds.colorPalette.teal[800],
           background: ds.colorPalette.white,
         },
       }}>
