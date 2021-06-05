6dad129d2801c68fecaa5b4466b4d8c54fd2527d
fengshangwuqi <fengshangwuqi@gmail.com>
2021-06-05 21:18:19 +0800

    perf: update app envs

---
+++ src-app/blog/config.ts
@@ -0,0 +1,10 @@
+export const ENVS = {
+  DEPLOY_BRANCH: "gh-pages",
+  META_AUTHOR: "枫上雾棋",
+  META_TITLE: "枫上雾棋的 blog",
+  META_DECS: "枫上雾棋的 blog",
+  SITE_URL: "https://blog.fengshangwuqi.com",
+  PATH_PREFIX: "/",
+  SOURCES: "posts,zhoubao",
+  SIZE: 6,
+};
---
+++ src-app/storybook/config.ts
@@ -0,0 +1,9 @@
+export const ENVS = {
+  DEPLOY_BRANCH: "sb-pages",
+  META_AUTHOR: "枫上雾棋",
+  META_TITLE: "枫上雾棋的 storybook",
+  META_DECS: "枫上雾棋的 storybook",
+  SITE_URL: "https://storybook.fengshangwuqi.com",
+  PATH_PREFIX: "/",
+  SOURCES: "src-core,src-components,utils,snippets",
+};
