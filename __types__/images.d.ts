4c9ce4e8bc33b2d94ab6548bb060bac2d76be1c1
FengShangWuQi <fengshangwuqi@gmail.com>
2021-12-11 18:08:48 +0800

    chore: deps udpate

--- __types__/images.d.ts
+++ __types__/images.d.ts
@@ -1,9 +1,6 @@
 declare module "*.svg" {
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement>
-  >;
-
-  export default ReactComponent;
+  const content: any;
+  export default content;
 }
 
 declare module "*.png" {
