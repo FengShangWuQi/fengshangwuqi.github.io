ff46f3a2de1fc200c519c719a91e82e6afb905b5
FengShangWuQi <fengshangwuqi@gmail.com>
2022-04-25 07:53:56 +0800

    perf: start script

--- scripts/start.ts
+++ scripts/start.ts
@@ -6,5 +6,9 @@ import { spawn } from "../utils/spawn";
   const action = argv[2] || "dev";
   const app = argv[3] || "blog";
 
+  if (argv[4]) return;
+  if (!["dev", "build", "release"].includes(action)) return;
+  if (!["blog", "storybook"].includes(app)) return;
+
   await spawn(`appkit ${action} ${app}`);
 })();
