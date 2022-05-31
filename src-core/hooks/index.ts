8eb00dcfcebad22c7b691fb39aedb273e9d1ea5e
FengShangWuQi <fengshangwuqi@gmail.com>
2022-05-31 18:46:17 +0800

    feat(sb): rm useIsomorphicLayoutEffect

--- src-core/hooks/index.ts
+++ src-core/hooks/index.ts
@@ -4,4 +4,3 @@ export * from "./useToggle";
 export * from "./useObservable";
 export * from "./useRect";
 export * from "./useVisibilitySensor";
-export * from "./useIsomorphicLayoutEffect";
--- src-core/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect, useLayoutEffect } from "react";
-
-/*
- * use useIsomorphicLayoutEffect in place of useLayoutEffect to avoid SSR warning
- */
-export const useIsomorphicLayoutEffect =
-  typeof window !== "undefined" ? useLayoutEffect : useEffect;
