692b10bf86a853d29760739b9f99b53169cc0b6e
FengShangWuQi <fengshangwuqi@gmail.com>
2022-09-18 16:01:39 +0800

    fix(script): build app

--- scripts/build/build-app.ts
+++ scripts/build/build-app.ts
@@ -4,10 +4,12 @@ import { getCIName } from "../../utils/ci";
 (async () => {
   if (getCIName() !== "Vercel") {
     [
-      "SENTRY_DSN",
       "GATSBY_ALGOLIA_APP_ID",
       "GATSBY_ALGOLIA_SEARCH_KEY",
       "ALGOLIA_ADMIN_KEY",
+      "GA_TRACKING_ID",
+      "SENTRY_DSN",
+      "GITHUB_TOKEN",
     ].forEach(e => delete process.env[e]);
   }
 
